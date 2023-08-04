f277d3219048907f511f2b8e921648d2fab72d2c
romina <rominaretamoso23@gmail.com>
2023-08-04 18:32:10 -0300

    formulario terminado

--- js/validacion.js
+++ js/validacion.js
@@ -1,12 +1,18 @@
 const formulario=document.getElementById('formulario');
 const inputs=document.querySelectorAll('#formulario input');
+const activacion =document.getElementById('btn-enviar');
+const mensaje_exito=document.getElementById('exito');
+
+
 const expresiones={
   nombre:/^[ a-zA-Z]{1,20}$/,
+  email:/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
   asunto:/^[ a-zA-Z]{1,10}$/,
   mensaje:/^[ a-zA-Z0-9]{1,100}$/
 }
 const campos = {
 	nombre: false,
+	email:false,
   	asunto:false,
   	mensaje:false
 }
@@ -14,6 +20,9 @@ const validarFormulario = (e) => {
 	switch (e.target.name) {
 		case "nombre":
 			validarCampo(expresiones.nombre, e.target, 'nombre');
+		break;
+		case "email":
+			validarCampo(expresiones.email, e.target, 'email');
 		break;
 		case "asunto":
 			validarCampo(expresiones.asunto, e.target, 'asunto');
@@ -23,10 +32,14 @@ const validarFormulario = (e) => {
 		break;
 	}
 }
+const val=(campos)=>{
+	if(campos.nombre && campos.email && campos.asunto && campos.mensaje){	
+		activacion.disabled = false;				
+	}
+ };
 
 const validarCampo = (expresion, input, campo) => {
-	if(expresion.test(input.value)){
-		console.log(campo);
+	if(expresion.test(input.value)){		
 		document.getElementById(`${campo}`).classList.remove('formulario__grupo-incorrecto');
 		document.getElementById(`${campo}`).classList.add('formulario__grupo-correcto');
 		document.querySelector(`#${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
@@ -37,6 +50,7 @@ const validarCampo = (expresion, input, campo) => {
 		document.querySelector(`#${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
 		campos[campo] = false;
 	}
+	val(campos);
 };
 
 inputs.forEach((input) => {
@@ -44,6 +58,15 @@ inputs.forEach((input) => {
 	input.addEventListener('blur', validarFormulario);
 });
 
-formulario.addEventListener('submit', (e) => {	
-	formulario.reset();
+formulario.addEventListener('submit', (e) => {
+	e.preventDefault();
+	formulario.reset();	
+	console.log("entro aqui");
+	mensaje_exito.classList.add('mensaje-exito-activo');	
+	setTimeout(() => {	
+		
+		mensaje_exito.classList.remove('mensaje-exito');	
+		
+	}, 10000);
+	
 });
